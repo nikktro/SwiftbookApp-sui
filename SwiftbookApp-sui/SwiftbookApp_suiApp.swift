6ee70b3d85a74e88ba70dc07ad3912a52857ef96
Nikolay Trofimov <nikktro@gmail.com>
2023-01-29 13:58:34 +0300

    Initial Commit

---
+++ SwiftbookApp-sui/SwiftbookApp_suiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftbookApp_suiApp.swift
+//  SwiftbookApp-sui
+//
+//  Created by Nikolay Trofimov on 29.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftbookApp_suiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
